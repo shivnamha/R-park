35a450800f61b8656668f902491b6dba27b28653
shivnamha <shivnamha@gmail.com>
2014-12-17 19:47:16 +0530

    Code for first test

---
+++ ser-data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from serial import Serial
+import subprocess
+
+ser = Serial('/dev/ttyUSB0', 9600, timeout=1)
+print("connected to: " + ser.portstr)
+ 
+while True:
+    # Read a line and convert it from b'xxx\r\n' to xxx
+    line = ser.readline().decode('utf-8')[:-2]
+ 
+    if line:  # If it isn't a blank line
+        print(line)
+        if line == '520':
+            subprocess.call(["xte", "key Up"])
+        elif line == '620':
+            subprocess.call(["xte", "key Down"])
+        elif line == '110':
+            break
+ 
+ser.close()
+
+
+
